7c1b00f2b419f6aed5a53a524d512d3b29ea2e97
AgaWojtasik <agawojtasik25@wp.pl>
2019-09-24 22:22:28 +0200

    New input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    username: '',
+    email: '',
+    pass: '',
+    accept: false,
+  }
+  handleChange = (e) => {
+
+
+    const name = e.target.name;
+    const type = e.target.type;
+
+    if (type === "text" || type === "password" || type === "email") {
+      const value = e.target.value;
+      const checked = e.target.checked;
+      this.setState({
+        [name]: value
+      })
+    } else if (type === "checkbox") {
+      const checked = e.target.checked;
+      this.setState({
+        [name]: checked
+      })
+    }
+
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    // console.log('działa')
+  }
   render() {
     return (
       <div className="App">
-        Formularz
+        <form onSubmit={this.handleSubmit} noValidate>
+          <label htmlFor="user">Twoje imię:
+            <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="email">Twój e-mail:
+            <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="password">Twoje hasło:
+            <input type="password" id="password" name="pass" value={this.state.pass} onChange={this.handleChange} />
+          </label>
+
+          <label htmlFor="accept">
+            <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} /> Wyrażam zgodę
+          </label>
+
+          <button>Zapisz się</button>
+        </form>
       </div>
     )
   }
