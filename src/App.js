93e1b490666c20c9fabe795dec93ab664199b73f
AgaWojtasik <agawojtasik25@wp.pl>
2019-09-26 00:51:37 +0200

    Validation

--- src/App.js
+++ src/App.js
@@ -8,10 +8,25 @@ class App extends Component {
     email: '',
     pass: '',
     accept: false,
+
+    errors: {
+      username: false,
+      email: false,
+      pass: false,
+      accept: false,
+    }
   }
+
+
+
+  messages = {
+    username_incorrect: 'Nazwa musi być dłuższa niż 10 znaków i nie może zawierać spacji',
+    email_incorrect: 'Brak @ w emailu',
+    pass_incorrect: 'Hasło musi mieć 8 znaków',
+    accept_incorrect: 'Nie potwierdzona zgoda',
+  }
+
   handleChange = (e) => {
-
-
     const name = e.target.name;
     const type = e.target.type;
 
@@ -32,28 +47,89 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault()
-    // console.log('działa')
+    const validation = this.formValidation()
+    // console.log(validation)
+
+    if (validation.correct) {
+      this.setState({
+        username: '',
+        email: '',
+        pass: '',
+        accept: false,
+
+        errors: {
+          username: false,
+          email: false,
+          pass: false,
+          accept: false,
+        }
+      })
+      // console.log('formularz wyslany');
+    } else {
+      this.setState({
+        errors: {
+          username: !validation.username,
+          email: !validation.email,
+          pass: !validation.pass,
+          accept: !validation.accept,
+        }
+      })
+    }
   }
+  formValidation = () => {
+    let username = false;
+    let email = false;
+    let pass = false;
+    let accept = false;
+    let correct = false;
+
+    if (this.state.username.length > 10 && this.state.username.indexOf(' ') === -1) {
+      username = true;
+    }
+    if (this.state.email.indexOf('@') !== -1) {
+      email = true;
+    }
+    if (this.state.pass.length === 8) {
+      pass = true;
+    }
+    if (this.state.accept) {
+      accept = true;
+    }
+    if (username && email && pass && accept) {
+      correct = true;
+    }
+    return ({
+      correct,
+      username,
+      email,
+      pass,
+      accept,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <form onSubmit={this.handleSubmit} noValidate>
           <label htmlFor="user">Twoje imię:
             <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange} />
+            {this.state.errors.username && <span>{this.messages.username_incorrect}</span>}
           </label>
 
           <label htmlFor="email">Twój e-mail:
             <input type="email" id="email" name="email" value={this.state.email} onChange={this.handleChange} />
+            {this.state.errors.email && <span>{this.messages.email_incorrect}</span>}
           </label>
 
           <label htmlFor="password">Twoje hasło:
             <input type="password" id="password" name="pass" value={this.state.pass} onChange={this.handleChange} />
+            {this.state.errors.pass && <span>{this.messages.pass_incorrect}</span>}
           </label>
 
           <label htmlFor="accept">
             <input type="checkbox" id="accept" name="accept" checked={this.state.accept} onChange={this.handleChange} /> Wyrażam zgodę
           </label>
-
+          {this.state.errors.accept && <span>{this.messages.accept_incorrect}</span>}
           <button>Zapisz się</button>
         </form>
       </div>
